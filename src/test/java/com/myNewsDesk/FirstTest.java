0ce60bff5277981b8ee45e0e758142cda67047d7
VladimirSokurenko <dp240191svv1@gmail.com>
2018-06-29 19:06:37 +0300

    init repo

---
+++ src/test/java/com/myNewsDesk/FirstTest.java
@@ -0,0 +1,23 @@
+package com.myNewsDesk;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTest extends WebDriverSetUp{
+
+    @Test
+    public void firstTest(){
+
+        getStaging();
+        logIn("test3", "123123qwe");
+
+
+    }
+
+
+
+}
---
+++ src/test/java/com/myNewsDesk/WebDriverSetUp.java
@@ -0,0 +1,47 @@
+package com.myNewsDesk;
+
+import org.junit.After;
+import org.junit.Before;
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverSetUp {
+
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty("chromedriver", "/home/alexander/IdeaProjects/my_first_test");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @After
+    public void close(){
+        driver.quit();
+    }
+
+    //journalist variables and methods
+
+    public void getStaging(){
+        driver.get("https://www.mnd-staging-sloboda.com/user/signin");
+    }
+
+    public void getStaging(){
+        driver.get("https://www.mnd-staging-sloboda.com/user/signin");
+    }
+
+    public void logIn(String username, String password){
+        driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys(username);
+        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys(password);
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+
+    }
+
+}
