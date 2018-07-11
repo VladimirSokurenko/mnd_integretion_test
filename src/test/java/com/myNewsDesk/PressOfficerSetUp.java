71d80ed0446cefccab22b7f39112c6bfb7b7cd6d
VladimirSokurenko <dp240191svv1@gmail.com>
2018-07-11 11:19:51 +0300

    presss officers classes

---
+++ src/test/java/com/myNewsDesk/PressOfficerSetUp.java
@@ -0,0 +1,34 @@
+package com.myNewsDesk;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
+import java.util.Set;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class PressOfficerSetUp {
+
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty("chromedriver", "/home/alexander/IdeaProjects/my_first_test");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+}
---
+++ src/test/java/com/myNewsDesk/PressOfficerTests.java
@@ -0,0 +1,6 @@
+package com.myNewsDesk;
+
+
+
+public class PressOfficerTests {
+}
